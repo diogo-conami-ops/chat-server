5333f7c6fbb11a75d7d7acc0deef2dea816c1abb
diogo-conami-ops <diogoconamidev@gmail.com>
2025-10-24 09:06:17 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({port:8080});
+
+let mensagensHistorico = []
+wss.on('connection', function connection(ws){
+    ws.on('error', console.error);
+
+    // envia historico de mensagem pra quem se conecta 
+    
+    const historico = {
+        tipo: "historico",
+        mensagens: mensagensHistorico
+    };
+        
+    
+    ws.on('message', function message(dados){
+
+        console.log(`o servidor recebeu essa mensagem: %s`, dados);
+        // implemetação de ping pong padrão do websocket pra manter a conexão dos clientes com o servidor.
+        if(dados=="ping") {
+            return;
+        }
+
+        if(dados.toString().endsWith("/limpar")){
+            mensagensHistorico.length = 0
+            return;
+        }
+        
+        mensagensHistorico.push(dados.toString())
+        
+        console.log("mensagens no historico: " + mensagensHistorico)
+        
+        wss.clients.forEach(cleinte => {
+            if(cleinte.readyState===WebSocket.OPEN){ // verifico se a conexão do cliente ainda é valida, assim não envio pra absolutamente todos, só pros ativos
+                cleinte.send(dados.toString());
+            }
+        });
+        
+        console.log("o servidor repasou a msg")
+        
+    });
+    ws.send(JSON.stringify(historico))
+})
+
+console.log('Server rodando na porta 8080');
+
